fc8e880e693927ff94f257c609ef85765ee7ff92
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-05 14:40:49 +0000

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,28 @@
+function requestCSV(f, c) {
+	return new CSVAJAX(f, c);
+}
 
+function CSVAJAX(filepath, callback) {
+	this.request = new XMLHttpRequest();
+	this.request.timeout = 10000;
+	this.request.open("GET", filepath, true);
+	this.request.parent = this;
+	this.callback = callback;
+	this.request.onload = function() {
+		var d = this.response.split('\n'); /*1st separator*/
+		var i = d.length;
+		while (i--) {
+			if (d[i] !== "")
+				d[i] = d[i].split(','); /*2nd separator*/
+			else
+				d.splice(i, 1);
+		}
+		this.parent.response = d;
+		if (typeof this.parent.callback !== "undefined")
+			this.parent.callback(d);
+	};
+	this.request.send();
+}
+
+var cols = requestCSV("../data/col.csv");
+console.log(cols);
