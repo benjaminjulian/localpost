9c5a5897f92f94f6a287cba4e1e3407a4a5714b5
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-05 15:03:07 +0000

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -20,10 +20,49 @@ function CSVAJAX(filepath, callback) {
 		return d;
 	};
 	this.request.send();
-};
+}
+
+function RGB2HSL(r, g, b) {
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	var max = Math.max(r, g, b);
+	var min = Math.min(r, g, b);
+	var c = max - min;
+	var lum = (max + min) / 2;
+	var hue;
+	var sat;
+	if (c == 0) {
+		hue = 0;
+		sat = 0;
+	} else {
+		sat = c / (1 - Math.abs(2 * lum - 1));
+		switch (max) {
+			case r:
+				var segment = (g - b) / c;
+				var shift = 0 / 60; // R° / (360° / hex sides)
+				if (segment < 0) { // hue > 180, full rotation
+					shift = 360 / 60; // R° / (360° / hex sides)
+				}
+				hue = segment + shift;
+				break;
+			case g:
+				var segment = (b - r) / c;
+				var shift = 120 / 60; // G° / (360° / hex sides)
+				hue = segment + shift;
+				break;
+			case b:
+				var segment = (r - g) / c;
+				var shift = 240 / 60; // B° / (360° / hex sides)
+				hue = segment + shift;
+				break;
+		}
+	}
+	return [hue * 60, sat * 100, lum * 100]; // hue is in [0,6], scale it up
+}
 
 var rows = requestCSV("../data/col.csv");
-
+console.log(rows);
 for (i = 0; i < rows.length; i++) {
 	console.log(rows[i]);
 }
