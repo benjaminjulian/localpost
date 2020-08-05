2512b7a469db687d08d491d415b7c6f9b84d9a89
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-05 16:14:59 +0000

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -64,7 +64,7 @@ function RGB2HSL(r, g, b) {
 }
 
 function processWeather(h, s, l, shutter, gain) {
-	if (shutter * gain < 8000 || l < 10) {
+	if (shutter * gain < 2000 || l < 10) {
 		return "Myrkur";
 	} else if (s < 12) {
 		if (shutter * gain < 100) {
