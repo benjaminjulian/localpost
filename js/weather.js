52a98873ef561b2e3842660b17e912d8c025e5b7
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-06 13:12:56 +0000

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -162,9 +162,8 @@ function processArray(lines) {
 	var last_data;
 	
 	for (i = lines.length; i >= 0; --i) {
-		console.log("lines # " + i.toString());
-		console.log(lines.length);
-		console.log(lines[i-1]);
+		if (typeof(lines[i]) == "undefined") continue;
+		
 		var HSL = RGB2HSL(lines[i][1], lines[i][2], lines[i][3]);
 		var current_weather = processWeather(HSL[0], HSL[1], HSL[2], lines[i][4], lines[i][5]);
 		
