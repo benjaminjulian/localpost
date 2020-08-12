1c0af3828c561aec550005fc9308bd8e67257aa0
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-12 14:49:47 +0000

    Update blueness-graph.js

--- js/blueness-graph.js
+++ js/blueness-graph.js
@@ -1,5 +1,58 @@
 var show_grouped = true;
 
+function blueSkyIndex(h, s, l, shutter, gain, stdh, stds, stdl) {
+	var daycolor = 10 * s / shutter;
+	var darkness = shutter * gain;
+	
+	var colclarity = Math.abs(l-50);
+	colclarity = (50 - colclarity) * s;
+
+	var h_dist = Math.abs(h-230);
+	var h_dist_ind = h_dist > 100 ? 0 : 100 - h_dist;
+	h_dist_ind *= s;
+
+	return h_dist_ind * (Math.sqrt(stdh) * s / 100 + stdl / 5 + stds / 1.5 + colclarity / 0.9 + daycolor / 0.2 + h_dist_ind / 10000) / Math.pow(darkness, 1.5);
+}
+
+function RGB2HSL(r, g, b) {
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	var max = Math.max(r, g, b);
+	var min = Math.min(r, g, b);
+	var c = max - min;
+	var lum = (max + min) / 2;
+	var hue;
+	var sat;
+	if (c == 0) {
+		hue = 0;
+		sat = 0;
+	} else {
+		sat = c / (1 - Math.abs(2 * lum - 1));
+		switch (max) {
+			case r:
+				var segment = (g - b) / c;
+				var shift = 0 / 60; // R° / (360° / hex sides)
+				if (segment < 0) { // hue > 180, full rotation
+					shift = 360 / 60; // R° / (360° / hex sides)
+				}
+				hue = segment + shift;
+				break;
+			case g:
+				var segment = (b - r) / c;
+				var shift = 120 / 60; // G° / (360° / hex sides)
+				hue = segment + shift;
+				break;
+			case b:
+				var segment = (r - g) / c;
+				var shift = 240 / 60; // B° / (360° / hex sides)
+				hue = segment + shift;
+				break;
+		}
+	}
+	return [hue * 60, sat * 100, lum * 100]; // hue is in [0,6], scale it up
+}
+
 function pad(num, size) {
 	var s = num + "";
 	while (s.length < size) s = "0" + s;
@@ -51,8 +104,9 @@ d3.csv(document.currentScript.getAttribute('filename'), function(error, data) {
 	data.forEach(function(d) {
 		d.date = parseDate(d.time);
 		d.hour = parseHour(d.time.substring(11, 19));
-		d.close = d.temp;
-		latest_temp = d.temp;
+		hsl = RGB2HSL(v_r, v_g, v_b);
+		d.close = blueSkyIndex(hsl[0], hsl[1], hsl[2], v_speed, v_gain, d.coldev, d.satdev, d.contrast)
+		latest_temp = d.close;
 		highest_temp = Math.max(latest_temp, highest_temp);
 		lowest_temp = Math.min(latest_temp, lowest_temp);
 	});
@@ -84,10 +138,10 @@ d3.csv(document.currentScript.getAttribute('filename'), function(error, data) {
 		.selectAll("stop")
 		.data([{
 			offset: "0%",
-			color: "#00CCFF"
+			color: "#AAAAAA"
 		}, {
 			offset: "100%",
-			color: "red"
+			color: "#0000FF"
 		}])
 		.enter().append("stop")
 		.attr("offset", function(d) {
