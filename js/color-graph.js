17e89293952566ec89610f1f61e78f8317a9c0c1
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-08-28 07:58:11 +0000

    Update color-graph.js

--- js/color-graph.js
+++ js/color-graph.js
@@ -148,9 +148,9 @@ d3.csv(document.currentScript.getAttribute('filename'), function(error, data) {
 		contrast = d.contrast * 1.0;
 		
 		d.puff = (Math.pow(edges, 2) * contrast / 130 + std_s) / Math.pow(gain, 2);
-		d.stratification = 3 * v / 2 * Math.abs(230 - h) / (Math.max(s, 1) * Math.max(std_h, 1) * Math.max(std_v, 1));
+		d.stratification = v  * Math.abs(230 - h) / (Math.max(s, 1) * Math.max(std_h, 1) * Math.max(std_v, 1));
 		d.darkness = Math.sqrt(speed * gain) / 10;
-		d.blueness = 2 * s * Math.pow(Math.max(0, 100 - Math.abs(230 - h)), 1/gain) / Math.max(50 * std_v * Math.pow(gain, 3),1) / (1+(Math.abs(speed-4000)+speed-4000)/500);
+		d.blueness = 1.4 * s * Math.pow(Math.max(0, 100 - Math.abs(230 - h)), 1/gain) / Math.max(50 * std_v * Math.pow(gain, 3),1) / (1+(Math.abs(speed-4000)+speed-4000)/500);
 		console.log(d);
 		console.log("blueness: " + d.blueness.toString());
 	});
