e507044e7078eb2f163990f10e2f96340c438ece
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2020-10-26 10:32:04 +0000

    Update temp-graph.js

--- js/temp-graph.js
+++ js/temp-graph.js
@@ -74,17 +74,39 @@ d3.csv(document.currentScript.getAttribute('filename'), function(error, data) {
 	
 	y.domain([lowest_temp, highest_temp]);
 	//y.domain([d3.min(data, function(d) { return d.close; }), d3.max(data, function(d) { return d.close; })]);
-	svg.append("linearGradient")
+	svg.append("linearGradientCold")
 		.attr("id", "line-gradient")
 		.attr("gradientUnits", "userSpaceOnUse")
 		.attr("x1", 0)
 		.attr("y1", y(lowest_temp))
 		.attr("x2", 0)
+		.attr("y2", y(0))
+		.selectAll("stop")
+		.data([{
+			offset: "0%",
+			color: "#50AA80"
+		}, {
+			offset: "100%",
+			color: "#00CCFF"
+		}])
+		.enter().append("stop")
+		.attr("offset", function(d) {
+			return d.offset;
+		})
+		.attr("stop-color", function(d) {
+			return d.color;
+		});
+	svg.append("linearGradientHot")
+		.attr("id", "line-gradient")
+		.attr("gradientUnits", "userSpaceOnUse")
+		.attr("x1", 0)
+		.attr("y1", y(0))
+		.attr("x2", 0)
 		.attr("y2", y(highest_temp))
 		.selectAll("stop")
 		.data([{
 			offset: "0%",
-			color: "#00CCFF"
+			color: "#CC8080"
 		}, {
 			offset: "100%",
 			color: "red"
